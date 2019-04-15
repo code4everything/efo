622df1008285715f0e25e4a40afced8a0a2a54a7
pantao <easepan@qq.com>
2019-04-15 20:09:40 +0800

    实现校验验证码，完善Swagger配置

--- efo-stand/efo-stand-web/src/main/java/org/code4everything/efo/stand/web/controller/CommonController.java
+++ efo-stand/efo-stand-web/src/main/java/org/code4everything/efo/stand/web/controller/CommonController.java
@@ -1,13 +1,13 @@
 package org.code4everything.efo.stand.web.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.code4everything.boot.bean.Response;
+import org.code4everything.boot.message.VerifyCodeUtils;
 import org.code4everything.boot.web.mvc.BaseController;
 import org.code4everything.efo.base.util.CommonUtils;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.mail.MessagingException;
 
@@ -16,7 +16,7 @@ import javax.mail.MessagingException;
  * @since 2019-04-11
  */
 @RestController
-@Api("公共接口")
+@Api(tags = "公共接口")
 public class CommonController extends BaseController {
 
     @PostMapping("/code/send/{email}")
@@ -25,5 +25,10 @@ public class CommonController extends BaseController {
         return CommonUtils.sendCode(this, email);
     }
 
-
+    @GetMapping("/code/validate/{email}")
+    @ApiOperation("校验验证码")
+    @ApiImplicitParam(name = "code", value = "验证码")
+    public Response<Boolean> validateCode(@PathVariable String email, @RequestParam String code) {
+        return parseBoolean("验证码正确", "验证码错误", VerifyCodeUtils.validate(email, code));
+    }
 }
